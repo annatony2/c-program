67712bb42c563148f0e121149b9a73321431acd4
annatony2 <annatony2028@cs.sjcetpalai.ac.in>
2025-02-05 14:31:23 +0530

    Add files via upload

---
+++ SequentialSearch.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int i,limit,search;
+  printf("Enter the number of elements :");
+  scanf("%d",&limit);
+  int num[limit];
+  for(i=0;i<limit;i++){
+   printf("Enter the element :");
+   scanf("%d",&num[i]);
+   }
+  printf("Enter the element to search :");
+  scanf("%d",&search);
+  for(i=0;i<limit;i++){
+   if (search==num[i]){
+   printf("\nPosition of %d :%d",num[i],i+1);
+   }
+  }
+  return 0;
+  }
+  
