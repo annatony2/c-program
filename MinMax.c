d4c1d0d5eb0a3a4d64444d6abce28581e9f8117d
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-02-16 22:11:55 +0530

    Maximum and Minimum

--- MinMax.c
+++ MinMax.c
@@ -1,7 +1,10 @@
+//author Anna Tony
+//05-02-2025
+//To find Maximum and Minimum
 #include<stdio.h>
 int main()
 {
-  int i,limit,largest,smallest;
+  int i,limit,max,min,maxPosition,minPosition;
   printf("Enter the number of elements :");
   scanf("%d",&limit);
   int num[limit];
@@ -9,16 +12,22 @@ int main()
    printf("Enter the element :");
    scanf("%d",&num[i]);
    }
-  largest=num[0];
-  smallest=num[0];
+  max=num[0];
+  min=num[0];
   for(i=0;i<limit;i++){
-   if (num[i]>largest){
-   largest=num[i];
+   if (num[i]>max){
+    max=num[i];
+    if(max==num[i]){
+        maxPosition=i+1;       
+    }
    }
-   if (num[i]<smallest){
-   smallest=num[i];
+   if (num[i]<min){
+    min=num[i];
+    if(max==num[i]){
+        minPosition=i+1;       
+    }
    }
   }
-  printf("Largest :%d\nSmallest :%d",largest,smallest);
+  printf("Largest  :%d \tPosition :%d\nSmallest :%d \tPosition :%d",max,min,maxPosition+1,minPosition+1);
   return 0;
   }
