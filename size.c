d27158e7bfbcbb35d3babad61ad08f11af489396
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-02-22 16:39:42 +0530

    size

---
+++ size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+ printf("\nsize of char=%ld", sizeof(char));
+ printf("\nsize of int=%ld", sizeof(int));
+ printf("\nsize of short int=%ld", sizeof(short int));
+ printf("\nsize of long int=%ld", sizeof(long int));
+ printf("\nsize of float=%ld", sizeof(float));
+ printf("\nsize of double=%ld", sizeof(double));
+}
