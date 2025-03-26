35ef1ae9739c3627f25f969b3d26048cd522fb5b
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-03-26 14:34:29 +0530

    Update New.c

--- New.c
+++ New.c
@@ -23,3 +23,30 @@ int main(){
 
     return 0;
 }
+
+#include <stdio.h>
+
+// Defining macros for constants
+#define PI 3.14159
+#define AREA_CIRCLE(r) (PI * (r) * (r))
+#define AREA_CYLINDER(r, h) (2 * PI * (r) * ((r) + (h)))
+
+int main() {
+    double radius, height;
+
+    // Input radius and height
+    printf("Enter the radius of the circle and cylinder: ");
+    scanf("%lf", &radius);
+    printf("Enter the height of the cylinder: ");
+    scanf("%lf", &height);
+
+    // Calculate areas using macros
+    double circle_area = AREA_CIRCLE(radius);
+    double cylinder_area = AREA_CYLINDER(radius, height);
+
+    // Display results
+    printf("Area of the circle: %.2lf\n", circle_area);
+    printf("Surface area of the cylinder: %.2lf\n", cylinder_area);
+
+    return 0;
+}
