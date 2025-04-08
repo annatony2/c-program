8a9a6c9582a2fb90305cc01c2e9ece72f3597ec5
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-04-08 22:45:46 +0530

    Update avgMark.c

--- avgMark.c
+++ avgMark.c
@@ -7,31 +7,34 @@
 struct Student {
     char name[50];
     int rollNo;
-    float marks[3],
+    float marks[3];
     float total;
     float average;
 };
 
 int main() {
     int n,i,j;
-    printf("Enter the number of students: ");
+    float overallTotal=0,overallAvg=0;
+    printf("Enter the number of students : ");
     scanf("%d",&n);
 
     struct Student s[n];
     for (i=0;i<n;i++){
         printf("\nEnter details for Student %d\n",i+1);
-        printf("Enter name: ");
-        scanf("%s", s[i].name);
-        printf("Enter roll number: ");
-        scanf("%d", &s[i].rollNo);
-        s[i].total = 0;
-        for (j = 0; j < 3; j++) {
-            printf("Enter marks for subject %d: ",j+1);
-            scanf("%f", &s[i].marks[j]);
-            s[i].total += s[i].marks[j];
+        printf("Enter name : ");
+        scanf("%s",s[i].name);
+        printf("Enter roll number : ");
+        scanf("%d",&s[i].rollNo);
+        s[i].total=0;
+        for (j=0;j<3;j++) {
+            printf("Enter marks for subject %d : ",j+1);
+            scanf("%f",&s[i].marks[j]);
+            s[i].total+=s[i].marks[j];
         }
-        s[i].average = s[i].total / 3;
+        s[i].average=s[i].total/3;
+        overallTotal+=s[i].total;
     }
+    overallAvg=overallTotal/(n*3);
     printf("\nStudent Details:\n");
     for (i = 0; i < n; i++) {
         printf("\nStudent %d\n", i + 1);
@@ -40,6 +43,6 @@ int main() {
         printf("Total Marks: %.2f\n", s[i].total);
         printf("Average Marks: %.2f\n", s[i].average);
     }
-
+    printf("\nOverall Average Marks : %.2f\n", overallAvg);
     return 0;
 }
