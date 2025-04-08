5a6251cb4efe58db191d549b6fde632afaa2b64b
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-04-08 10:46:37 +0530

    Add files via upload

---
+++ file1.c
@@ -0,0 +1,20 @@
+/*Author       : Anna Tony
+  Date         : 08-04-2025
+  Version      : 1.0  */
+#include<stdio.h>
+int main(){
+  FILE  *fp;
+  char ch;
+  fp=fopen("file.txt","w");
+  while((ch=getc(stdin))!='z'){
+    putc(ch,fp);
+  }
+  fclose(fp);
+  fp=fopen("file.txt","r");
+  while((ch=getc(fp))!=EOF){
+    printf("%c",ch);
+  }
+  fclose (fp);
+  
+  return 0;
+  }
