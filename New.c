3e1dd594318acd88a7b9c7514ef277a4d7f1f8fe
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-04-09 12:13:04 +0530

    Update New.c

--- New.c
+++ New.c
@@ -4,123 +4,38 @@
   Version      : 1.0  */
 #include <stdio.h>
 
-#define SWAP(a, b) { int temp=a;a=b;b=temp; }
-
-int main(){
-    int x, y;
-    printf("Enter two integers : ");
-    printf("x = ");
-    scanf("%d", &x);
-    printf("y = ");
-    scanf("%d", &y);
-    printf("\nBefore swapping:\n");
-    printf("x = %d, y = %d\n", x, y);
-
-    SWAP(x, y);
-
-    printf("\nAfter swapping:\n");
-    printf("x = %d, y = %d\n", x, y);
-
-    return 0;
-}
-
-#include <stdio.h>
-struct Student {
-    char name[50];
-    int rollNo;
-    float marks[3],
-    float total;
-    float average;
-};
-
 int main() {
-    int n, i, j;
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
+    FILE *inFile, *oddFile, *evenFile;
+    int num;
 
-    struct Student s[n];
-    for (i = 0; i < n; i++) {
-        printf("\nEnter details for Student %d\n", i + 1);
-        printf("Enter name: ");
-        scanf("%s", s[i].name);
-        printf("Enter roll number: ");
-        scanf("%d", &s[i].rollNo);
-        s[i].total = 0;
-        for (j = 0; j < 3; j++) {
-            printf("Enter marks for subject %d: ", j + 1);
-            scanf("%f", &s[i].marks[j]);
-            s[i].total += s[i].marks[j];
-        }
-        s[i].average = s[i].total / 3.0;
-    }
-    printf("\nStudent Details:\n");
-    for (i = 0; i < n; i++) {
-        printf("\nStudent %d\n", i + 1);
-        printf("Name: %s\n", s[i].name);
-        printf("Roll No: %d\n", s[i].rollNo);
-        printf("Total Marks: %.2f\n", s[i].total);
-        printf("Average Marks: %.2f\n", s[i].average);
+    // Open files
+    inFile = fopen("number.txt", "r");
+    oddFile = fopen("odd.txt", "w");
+    evenFile = fopen("even.txt", "w");
+
+    // Check if input file opened successfully
+    if (inFile == NULL) {
+        printf("Error: Cannot open number.txt\n");
+        return 1;
     }
 
-    return 0;
-}
-#include<stdio.h>
-#define MAX 10
-typedef struct{
-    int rollno;
-    char name[20];
-    int m[3];
-    int totalMarks;
-}Student;
-//Function to read the student details
-void read(Student s[], int n){
-    for(int i=0;i<n;i++){
-        printf("\nEnter the Roll No:");
-        scanf("%d",&s[i].rollno);
-        printf("\nEnter the name of the student");
-        scanf("%s", s[i].name);
-        printf("\nEnter the marks:");
-        scanf("%d%d%d",&s[i].m[0],&s[i].m[1],&s[i].m[2]);
-
-    }
-}
-//Function to compute the total marks
-void calculateTotal(Student s[],int n){
-    for(int i=0;i<n;i++){
-        s[i].totalMarks = s[i].m[0]+s[i].m[1]+s[i].m[2];
-    }
-}
-//Function to create the rank list
-void prepareRankList(Student s[], int n){
-    Student temp;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(s[j].totalMarks<s[j+1].totalMarks){
-                //swap students
-                temp= s[j];
-                s[j] = s[j+1];
-                s[j+1]=temp;
-            }
+    // Read numbers using !feof()
+    while (!feof(inFile)) {
+        fscanf(inFile, "%d", &num);
+        if (!feof(inFile)) {  // To avoid reading garbage at the end
+            if (num % 2 == 0)
+                fprintf(evenFile, "%d\n", num);  // Write even
+            else
+                fprintf(oddFile, "%d\n", num);   // Write odd
         }
     }
-}
-//Function to print the RankList
-void printRankList(Student s[], int n){
-    printf("\nRank\tRollNumber\tName\tM1\tM2\tM3\tTotalMarks\n");
-    for(int i=0;i<n;i++){
-        printf("%d\t%d\t\t%s\t%d\t%d\t%d\t%d\n", i+1, s[i].rollno,s[i].name,s[i].m[0],s[i].m[1],s[i].m[2],s[i].totalMarks);
 
-    }
-}
+    // Close all files
+    fclose(inFile);
+    fclose(oddFile);
+    fclose(evenFile);
 
-int main(){
-    Student s[MAX];
-    int n;
-    printf("\nEnter the number of students(MAx:10)");
-    scanf("%d",&n);
-    read(s,n);
-    calculateTotal(s,n);
-    prepareRankList(s,n);
-    printRankList(s,n);
+    printf("Numbers have been separated into odd.txt and even.txt\n");
+
     return 0;
 }
