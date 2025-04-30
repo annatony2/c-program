798a68006f69f05369a94bd5b89360958fde0023
Anna Tony <annatony2028@cs.sjcetpalai.ac.in>
2025-04-30 14:46:57 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,21 @@
+/*Author       : Anna Tony
+  Date         : 30-04-2025
+  Version      : 1.0  */
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int *array;
+  int n;
+  printf("Enter the no of elements:");
+  scanf("%d",&n);
+  array=(int *) malloc(n*sizeof(int));
+  printf("\n Enter the elements : ");
+  for (int i=0;i<n;i++){
+    scanf("%d",array+i);
+  }
+  for (int i=0;i<n;i++){
+    printf("%d",*(array+i));
+  }
+  return 0;
+  }           
+   
